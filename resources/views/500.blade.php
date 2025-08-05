6e9fe75a4db457c2c050c7cd3845867ef3be479b
allmight777 <agoliganange15@gmail.com>
2025-08-05 14:43:23 +0100

    Update 500.blade.php

--- resources/views/500.blade.php
+++ resources/views/500.blade.php
@@ -1,16 +1,16 @@
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-    <meta charset="UTF-8">
-    <title>Erreur 500</title>
+    <meta charset="UTF-8" />
+    <title>Erreur interne du serveur</title>
 </head>
 <body>
     <h1>Erreur interne du serveur</h1>
     <p>Une erreur est survenue. Merci de réessayer plus tard.</p>
 
-    {{-- Optionnel : affiche le message si le debug est activé --}}
     @if(config('app.debug'))
         <p style="color:red;">{{ $exception->getMessage() }}</p>
     @endif
 </body>
 </html>
+
