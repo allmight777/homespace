51c9ee470017c02e94a38b8e4b175c27e9f45c75
allmight777 <agoliganange15@gmail.com>
2025-08-05 12:28:59 +0100

    Update auth.php

--- routes/auth.php
+++ routes/auth.php
@@ -13,10 +13,6 @@ use Illuminate\Support\Facades\Route;
 
 
 
-Route::get('/', function () {
-    return view('welcome');
-})->name('welcome');
-
 Route::middleware('guest')->group(function () {
     Route::get('register', [RegisteredUserController::class, 'create'])
         ->name('register');
