4cd52758df703f49579065cba9d3fe9107cbbc23
allmight777 <agoliganange15@gmail.com>
2025-08-05 12:39:08 +0100

    Update 500.blade.php

--- resources/views/500.blade.php
+++ resources/views/500.blade.php
@@ -1,15 +1,16 @@
-@extends('layouts.welcome') 
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Erreur 500</title>
+</head>
+<body>
+    <h1>Erreur interne du serveur</h1>
+    <p>Une erreur est survenue. Merci de réessayer plus tard.</p>
 
-@section('title', 'Erreur Serveur')
-
-@section('content')
-    <div style="text-align:center; margin-top: 50px;">
-        <h1>Erreur 500</h1>
-        <p>Une erreur interne est survenue. Veuillez réessayer plus tard.</p>
-
-        {{-- Facultatif : afficher l’erreur si APP_DEBUG est activé --}}
-        @if(config('app.debug'))
-            <p style="color:red;">{{ $exception->getMessage() }}</p>
-        @endif
-    </div>
-@endsection
+    {{-- Optionnel : affiche le message si le debug est activé --}}
+    @if(config('app.debug'))
+        <p style="color:red;">{{ $exception->getMessage() }}</p>
+    @endif
+</body>
+</html>
