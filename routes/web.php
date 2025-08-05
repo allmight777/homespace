43dc0222e4991503086fe49c112e3cc5a18fa444
allmight777 <agoliganange15@gmail.com>
2025-08-05 14:42:55 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Symfony\Component\HttpKernel\Exception\HttpException;
 Route::get('/', function () {
     try {
         // Ton code normal
-        return view('welcome');
+        return view('bienvenue');
     } catch (\Throwable $exception) {
         \Log::error($exception);
         return response()->view('500', ['exception' => $exception], 500);
